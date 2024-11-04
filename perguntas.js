cbfc3e37322d9d21e4437f1dcc90367e2ce7bbb2
ADRIAN <148337599+adrianxit@users.noreply.github.com>
2024-11-04 08:07:53 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Inglês',
+    'como falar bom dia em inglês?',
+    'good morning'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvarez Cabral'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é o delta?',
+    'É um elemento matematico utilizado em formúlas'
+)
